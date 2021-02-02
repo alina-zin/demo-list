d87a41b595fafa8b2543819959a9bd3b6b01dce8
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-02 14:35:44 +0200

    list start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+  const [names, setNames] = useState([]);
+
+  useEffect(() => {
+    setNames(['Jouni', 'Pekka', 'Ilkka']);
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Names</h3>
+      <table>
+        {names.map((name, index) => (
+          <tr key = {index}><td>{name}</td></tr>
+        ))
+        }
+      </table>
     </div>
   );
 }
